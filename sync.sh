7cbdfbc30d362b3bf6352dc77dc83f15e3656888
James Addyman <addyman@me.com>
2013-05-22 22:11:57 +0100

    updated opus submodule, personalised build, config, and sync scripts, updated .gitignore

--- sync.sh
+++ sync.sh
@@ -6,11 +6,15 @@ source config.sh
 
 SCRIPT_DIR=$( (cd -P $(dirname $0) && pwd) )
 DIST_DIR_BASE=${DIST_DIR_BASE:="$SCRIPT_DIR/dist"}
+OPUS_VER="2040606f4a1d3b230bdf00e1b7e4427df8dcdd3b"
+
+echo ${OPUS_VER}
 
 git submodule update --init opus
 cd opus
+git checkout master
+git pull origin master
 git checkout ${OPUS_VER}
-git pull 
 cd ..
 
 for ARCH in $ARCHS
